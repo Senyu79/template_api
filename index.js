2a6827657cda08527a6a1af8073abddd9c9277d1
Студент группы IS <is@sielom.local>
2025-04-24 17:40:05 +0500

    dfsd

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const  { createClient } = require ('@supabase/supabase-js')
+
+
+const supabase = createClient('https://vjdussiriyrxoxsjhzsd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqZHVzc2lyaXlyeG94c2poenNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0NTk3MTEsImV4cCI6MjA0ODAzNTcxMX0.b6p5OaqaXBrMhebhSBFlwpZG5kFqhpLGwQQu4AFxZ00')
+
+
+
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/adress', async (req, res) => {
+  const { data, error } = await supabase
+  .from('adress')
+  .select()
+  res.send(data)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, ()=> {
+  console.log('Example app istening on port ${port}')
 })
